838303cd995410098500744537513c3f22302142
André <andre2ar@outlook.com>
2019-11-21 16:39:38 -0300

    Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Logo.module.css';
+
+import heraBankLogo from '../../assets/images/logo.png';
+
+const Logo = () => (
+    <div className={classes.Logo}>
+        <img src={heraBankLogo} alt={"Hera Bank Logo"}/>
+        <span className={classes.brand}>Hera Bank</span>
+    </div>
+);
+
+export default Logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
+import Logo from "../../Logo/Logo";
 
 const Toolbar = () => (
     <header className={classes.Toolbar}>
-
+        <div>Menus</div>
+        <Logo/>
     </header>
 );
 
