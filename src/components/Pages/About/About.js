ac3067e29e9909e19c61e1b46568d0a3b7ff5a04
André <andre2ar@outlook.com>
2019-11-25 14:57:57 -0300

    Services

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -10,6 +10,11 @@ import Background from "../../UI/background/background";
 const About = () => (
     <div className={classes.About} style={Background(background)}>
         <Title>Quem somos</Title>
+        <p>
+            O HERA BANK é uma Instituição de Pagamentos que agrega além da conta digital,
+            máquina de cartão para sua empresa e cartão de crédito pré-pago. Um sistema
+            de serviços completos para você e seu negócio
+        </p>
     </div>
 );
 
