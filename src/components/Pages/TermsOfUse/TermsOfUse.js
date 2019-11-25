fd8bde68bdf99781e47ac09246d343d5f8a23f7d
André <andre2ar@outlook.com>
2019-11-25 16:46:18 -0300

    Terms of use

--- src/components/Pages/TermsOfUse/TermsOfUse.js
+++ src/components/Pages/TermsOfUse/TermsOfUse.js
@@ -1,10 +1,245 @@
 import React from 'react';
 import background from "../../../assets/images/12-use-terms.jpg";
 import Background from "../../UI/background/background";
+import Title from "../../UI/Title/Title";
+
+import classes from "./TermsOfUse.module.css";
 
 const TermsOfUse = () => (
-    <div style={Background(background)}>
+    <div className={classes.TermsOfUse} style={Background(background)}>
+        <Title>Termos e condições de Uso</Title>
 
+        <div className={classes.Contract}>
+            <p>
+                HERA BANK PAGAMENTOS (denominada “HERA BANK ”), inscrita no CNPJ sob nº CNPJ 13.566.137/0001-50,
+                com sede e domicílio na Av. Deputado Jamel Cecílio  N: 2690 sala 412 Edf. Metropolitan
+                Torre Tokio : Jardim Goiás, na cidade de Goiânia, Goiás, CEP 74810-100,  disponibilizada através do
+                SITE hospedado sob o domínio http://www.herabank.com (denominado “SITE”), e o USUÁRIO, celebram o
+                presente TERMO & CONDIÇÕES DE USO E SERVIÇOS (denominado “TC”), que define as condições pelas quais
+                o USUÁRIO poderá acessar e utilizar as funcionalidades do sistema de propriedade da NEXTCOIN . Faz
+                parte integrante e indissociável deste instrumento a POLÍTICA DE PRIVACIDADE.
+            </p>
+
+            <h2>Aceite</h2>
+            <ol>
+                <li>
+                    A utilização dos SERVIÇOS implica na mais alta compreensão, aceitação e vinculação automática
+                    do USUÁRIO ao TC. Ao fazer uso de quaisquer dos SERVIÇOS oferecidos, o USUÁRIO concorda em
+                    respeitar e seguir todas e quaisquer diretrizes dispostas neste TC e seus ANEXOS e estar de
+                    acordo com os preceitos legais.
+                </li>
+
+                <li>
+                    Ao manifestar o “Aceite”, o USUÁRIO declara expressamente que leu, entendeu e compreendeu
+                    todos os direitos e obrigações determinados no TC, e ainda, concorda e se sujeita a todas as
+                    disposições do presente instrumento e dos seus ANEXOS.
+                </li>
+
+                <li>
+                    A utilização da HERA BANK  e dos SERVIÇOS, mesmo que parcial ou a título de teste, indica
+                    que o USUÁRIO concorda com o presente TC.
+                </li>
+
+                <li>
+                    Este TC poderá sofrer alterações periódicas, seja por questões legais ou estratégicas da
+                    HERA BANK . O USUÁRIO desde já concorda e reconhece que é de sua única e inteira responsabilidade
+                    a verificação periódica deste TC. O HERA BANK poderá, por mera liberalidade, informar ao USUÁRIO
+                    sobre alterações significativas no TC, através de avisos aos USUÁRIOS.
+                </li>
+
+                <li>
+                    Caso tenha alguma objeção a quaisquer itens, diretrizes ou alterações posteriores deste TC,
+                    ou, ainda, caso fique insatisfeito com os SERVIÇOS oferecidos pela HERA BANK , por qualquer
+                    motivo que seja, o USUÁRIO poderá cessar o uso, observado o disposto neste TC.
+                </li>
+
+                <li>
+                    Para utilizarem os SERVIÇOS, os USUÁRIOS deverão realizar um cadastro prévio e responderão
+                    pela veracidade e atualização dos dados informados no cadastro.
+                </li>
+            </ol>
+
+            <h2>Concessão da Licença de Uso</h2>
+
+            <ol>
+                <li>
+                    O HERA BANK , proprietária dos direitos do software objeto deste TC, neste ato e pela melhor
+                    forma de direito, outorga ao USUÁRIO uma licença de uso, em caráter não exclusivo e não
+                    transferível, disponibilizado para utilização via internet – em Exchange online.
+                </li>
+
+                <li>
+                    O USUÁRIO declara-se integralmente responsável pelas informações inseridas no HERA BANK, pelo
+                    cadastramento, permissões, senhas e modo de utilização. O HERA BANK , em hipótese alguma, será
+                    responsável pelo conteúdo (informações, senhas, cópias de informações etc.) oferecido pelo
+                    USUÁRIO, não sendo, portanto, estas informações revisadas nem divulgadas em momento algum. A
+                    responsabilidade pelas informações é sempre do USUÁRIO.
+                </li>
+            </ol>
+
+            <h2>Cadastro e Validação do Cadastro</h2>
+
+            <ol>
+
+                <li>
+                    Para a utilização de quaisquer dos SERVIÇOS, é necessária a realização do cadastro
+                    , em que o USUÁRIO deverá informar um e-mail válido e criação de login e senha.
+                </li>
+
+                <li>
+                    Para a utilização dos SERVIÇOS de depósito de valores em reais e retirada de valores
+                    em reais, o USUÁRIO deverá validar sua conta utilizando o número de seu Cadastro Nacional
+                    de Pessoa Física (CPF), uma imagem de documento com foto e comprovante de residência com
+                    data máxima de 3 (três) meses anteriores.
+                </li>
+
+                <li>
+                    O cadastro de pessoas jurídicas como USUÁRIO deverá ser realizado necessariamente por um
+                    representante legal devidamente autorizado para tanto.
+                </li>
+
+                <li>
+                    O cadastro de indivíduos com menos de 18 (dezoito) anos ocorrerá mediante o fornecimento
+                    de número do seu CPF e do CPF do responsável, que assumirá a responsabilidade pelos atos,
+                    serviços contratados, resgate de prêmios e todo e qualquer ato praticado pelo cadastrado
+                    com menos de 18 anos.
+                </li>
+
+                <li>
+                    Ao preencher o formulário de cadastro, o USUÁRIO ingressa no banco de dados da HERA BANK,
+                    autorizando   o  HERA BANK o acesso a referidos dados disponibilizados.
+                </li>
+
+                <li>
+                    Sendo constatada irregularidade no preenchimento do cadastro, omissão de informações, dados
+                    equivocados ou preenchimentos errôneos, o HERA BANK  poderá suspender o acesso do USUÁRIO
+                    à área do cadastro até que a situação se regularize, não se responsabilizando pelo salvamento
+                    automático de benefícios acumulados ou pelo salvamento de cadastro anterior.
+                </li>
+
+                <li>
+                    Os dados pessoais, de acesso e de transações dos USUÁRIOS são protegidos pela Política de Privacidade.
+                </li>
+
+                <li>
+                    O USUÁRIO garante que todas as informações prestadas são válidas e precisas e que a senha
+                    determinada é pessoal e intransferível. A atualização dos dados informados também é de
+                    responsabilidade do USUÁRIO e este está ciente de que a precisão e veracidade destes dados são
+                    imprescindíveis à efetivação dos SERVIÇOS.
+                </li>
+
+                <li>
+                    O HERA BANK se reserva o direito de recusar qualquer solicitação de cadastro e de suspender
+                    ou cancelar um cadastro previamente aceito nos casos (i) de violação de qualquer das disposições
+                    deste TC, (ii) impossibilidade de verificação da identidade do USUÁRIO ou constatação de
+                    falsidade em qualquer das informações por ele fornecidas, (iii) prática pelo USUÁRIO de atos
+                    fraudulentos ou dolosos ou a adoção de qualquer comportamento que, a critério da HERA BANK ,
+                    seja incompatível com os objetivos do SITE, com a urbanidade com outros USUÁRIOS ou que possam,
+                    de qualquer modo, causar danos a terceiros ou ao SITE.
+                </li>
+
+                <li>
+                    O USUÁRIO aceita e concorda que toda e qualquer transação deve ser efetuada tão e somente pelo
+                    titular do cadastro.
+                </li>
+
+                <li>
+                    O USUÁRIO é o único e exclusivo responsável por manter em segurança e sigilo seu login e senha,
+                    evitando assim o uso não autorizado de seu cadastro. O USUÁRIO deverá avisar imediatamente a
+                    HERA BANK, através do e-mail atendimento@HERABANK.COM, em caso de suspeita de acesso indevido,
+                    ou qualquer atividade que possa dar ensejo a tal, para que a HERA BANK  possa tomar as devidas
+                    providências.
+                </li>
+            </ol>
+
+            <h2>Funcionamento, Regras de Utilização dos Serviços, Limites, Restruições e Responsabilidades</h2>
+
+            <ol>
+                <li>
+                    Após conclusão do cadastro pelo USUÁRIO, é criada uma CONTA DIGIRAL para este, que poderá ser
+                    acessada, consultada e transacionada através do uso de sua senha e login.
+                </li>
+
+                <li>
+                    A CONTA DIGITAL do USUÁRIO é a conta criada automaticamente para o USUÁRIO, após a validação
+                    de seu cadastro.
+                </li>
+
+                <li>
+                    As transações realizadas entre os USUÁRIOS são de inteira responsabilidade dos USUÁRIOS.
+                </li>
+
+                <li>
+                    A HERA BANK poderá utilizar determinados parâmetros próprios para estabelecer determinados
+                    limites.
+                </li>
+
+                <li>
+                    A HERA BANK poderá, a seu exclusivo critério, aumentar e diminuir os limites inicialmente
+                    estipulados.
+                </li>
+
+                <li>
+                    O USUÁRIO se compromete e é o único responsável por não utilizar os SERVIÇOS para fins
+                    diretos ou indiretos de (i) infringir qualquer lei, regulamento, este TC, nem praticar
+                    atos contrários à moral e aos bons costumes; (ii) praticar lavagem de dinheiro; e/ou (iii)
+                    financiar atividades e/ou organizações que envolvam terrorismo, máfia, tráfico de drogas,
+                    pessoas e/ou órgãos humanos.
+                </li>
+            </ol>
+
+            <h2>Restrições ao Uso</h2>
+            <ol>
+                <li>
+                    O presente TC é regido pelas disposições da Lei 9.609/98 e Lei 9.610/98, ficando os
+                    infratores sujeitos às penas dos crimes previstos no art. 12 da Lei 9.609/98, sem prejuízo da
+                    responsabilidade civil pelos danos eventualmente causados pelo uso e distribuição de cópias não
+                    autorizadas do HERA BANK  ou por qualquer outra violação aos direitos decorrentes da
+                    propriedade do mesmo, incluindo, mas não se limitando, o acesso de terceiros não autorizados.
+                </li>
+            </ol>
+
+            <h2>Considerações Finais</h2>
+
+            <ol>
+                <li>
+                    A incapacidade ou a mera liberalidade em não fazer cumprir quaisquer disposições deste TC
+                    não constitui renúncia de direitos, podendo ser exigido o seu cumprimento em qualquer momento.
+                </li>
+
+                <li>
+                    Caso qualquer disposição deste TC for considerada inexequível ou inválida, essa disposição será
+                    isoladamente invalidada e não atingirá o restante das disposições destes TC.
+                </li>
+
+                <li>
+                    Este TC e a relação entre as Partes serão regidos pelas leis da República Federativa do Brasil.
+                    As Partes elegem o Foro da Comarca de GOIANIA, GOIAS, como sendo o único competente para dirimir
+                    quaisquer litígios e/ou demandas que venham a envolver as Partes em relação aos SERVIÇOS.
+                </li>
+
+                <li>
+                    A tolerância de uma Parte com a outra quanto ao descumprimento de qualquer uma das obrigações
+                    assumidas neste TC não implicará em novação ou renúncia de direito. A Parte tolerante poderá,
+                    a qualquer tempo, exigir da outra Parte o fiel e cabal cumprimento deste TC.
+                </li>
+
+                <li>
+                    O presente TC vincula as Partes e seus sucessores a qualquer título.
+                </li>
+
+                <li>
+                    A inviabilidade da execução ou invalidade, ainda que em virtude de decisão judicial, de alguma
+                    cláusula, não afetará a validade das demais cláusulas deste TC, que permanecerá válido e executável
+                    conforme seus termos.
+                </li>
+
+                <li>
+                    O HERA BANK se reserva o direito de modificar o presente TC a qualquer tempo, sendo o USUÁRIO
+                    comunicado de tais alterações, mediante o aceite das novas disposições.
+                </li>
+            </ol>
+        </div>
     </div>
 );
 
