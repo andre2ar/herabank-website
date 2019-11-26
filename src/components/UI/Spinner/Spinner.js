fe59373a1a0c926abecf334e6c153943edafdae9
André <andre2ar@outlook.com>
2019-11-26 10:45:44 -0300

    Spinner

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import classes from './Spinner.module.css';
 
-const Spinner = () => <div className={classes.Loader}>Carregando...</div>;
+const Spinner = () => (
+    <div className={classes.Spinner}>
+        <div className={classes.Loader}>Carregando...</div>
+    </div>
+);
 
 export default Spinner;
