88a20e3e2ecc67385c32bd1d8a464c47c83ecc43
André <andre2ar@outlook.com>
2019-11-27 13:53:55 -0300

    Text

--- src/components/Pages/Service/servicesList.js
+++ src/components/Pages/Service/servicesList.js
@@ -9,7 +9,7 @@ const servicesList = {
     },
     "credit-card-machine": {
         title: 'Maquinha',
-        details: ''
+        details: "Para facilitar a sua vida, com agilidade e praticidade. Nosso processo de abertura e validação de conta é automatizado, em instantes você estará com a maquininha a caminho de seu estabelecimento"
     },
     "credit-card": {
         title: 'Cartão Pré-pago Internacional',
